3eb819b1a40e6b4ee318e3b42017b1f4e3a0f983
Dev_Liam <johnbhjbg@gmail.com>
2021-05-22 19:22:48 +0900

    new init command added

--- init.sh
+++ init.sh
@@ -2,4 +2,7 @@ sudo apt update -y
 sudo apt install openjdk-11-jdk-headless -y
 sudo apt install wget -y
 sudo apt install unzip -y
+sudo apt install software-properties-common
 sudo add-apt-repository ppa:cwchien/gradle
+sudo apt update -y
+sudo apt install gradle
